b60066c3fbf5b8ebfe4dea14782501bb3e6b9678
Paul Filby <paulfilby@gmail.com>
2014-05-15 17:06:47 +0100

    almost done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,9 +3,11 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	public float speed = 10.0f;
+	private int count = 0;
+	public GUIText countText;
 	// Use this for initialization
 	void Start () {
-	
+		UpdateCount();
 	}
 	
 	// Update is called once per frame
@@ -17,5 +19,16 @@ public class PlayerController : MonoBehaviour {
 		float moveVertical = Input.GetAxis ("Vertical");
 		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
 		rigidbody.AddForce (movement*speed*Time.deltaTime);
+	}
+	void OnTriggerEnter(Collider other){
+		 if (other.gameObject.tag == "Pickup") {
+			other.gameObject.SetActive(false);
+			count++;
+			UpdateCount();
+		}
+	}
+
+	void UpdateCount() {
+		countText.text = count.ToString();
 	}
 }
