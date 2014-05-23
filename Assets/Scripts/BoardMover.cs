3618036bf680fd7091f2e0d0c206ad525704a92b
Paul Filby <paulfilby@gmail.com>
2014-05-23 17:15:36 +0100

    added tilt table

---
+++ Assets/Scripts/BoardMover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardMover : MonoBehaviour {
+	public float speed = 0.001f;
+	private float zRot = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//zRot= zRot+ speed;
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		transform.Rotate(moveVertical,0,moveHorizontal*-1);
+	}
+}
